23280dde6037161aff8f4fc75df74065e385b912
taijulsir <princetaijul123@gmail.com>
2025-08-09 02:15:59 +0600

    added ;user online &  last Active time in user model

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   image: { type: String, required: true },
+  lastActive: { type: Date, default: Date.now },
 });
 
 userSchema.pre('save', async function (next) {
